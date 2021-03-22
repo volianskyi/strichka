6593ddbeff58bb03ab4f9feaa066062d9e29d52a
Ihor <volyanskiy.i@gmail.com>
2021-03-22 22:51:35 +0200

    Updated pom and exc handling

--- src/main/java/com/strichka/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/strichka/exception/ApplicationExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.strichka.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +17,13 @@ public class ApplicationExceptionHandler {
         return getModelAndView(request, HttpStatus.NOT_FOUND, exception);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    public ModelAndView accessForbiddenExceptionHandler(HttpServletRequest request) {
+        return getModelAndView(request, HttpStatus.FORBIDDEN, "Access Denied");
+    }
+
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView internalServerErrorHandler(HttpServletRequest request, Exception exception) {
@@ -26,6 +34,13 @@ public class ApplicationExceptionHandler {
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("code", httpStatus.value() + " / " + httpStatus.getReasonPhrase());
         modelAndView.addObject("message", exception.getMessage());
+        return modelAndView;
+    }
+
+    private ModelAndView getModelAndView(HttpServletRequest request, HttpStatus httpStatus, String message) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("code", httpStatus.value() + " / " + httpStatus.getReasonPhrase());
+        modelAndView.addObject("message", message);
         return modelAndView;
     }
 
